c13c039d0712cae2da1b1bfd92cb6a97eb51df0f
Shadow-nx <32244138+Shadow-nx@users.noreply.github.com>
2018-03-23 17:50:05 +0300

    Update vector.cpp

--- sources/vector.cpp
+++ sources/vector.cpp
@@ -37,7 +37,7 @@ vector_t & vector_t::operator =(vector_t const & other)
 
 bool vector_t::operator ==(vector_t const & other) const
 {
-  if (tlsize_ != other.size_) 
+  if (size_ != other.size_) 
 	  return false;  
     for (int i = 0; i < size_; i++) 
       if (elements_[i] != other.elements_[i]) 
